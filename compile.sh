5aecd55239853901041f3272f83713d92132f163
Martin Kustermann <kustermann@google.com>
2024-06-06 11:09:34 +0200

    Move benchmarks{,-out} -> micro-benchmarks{,-out}

--- compile.sh
+++ compile.sh
@@ -3,17 +3,17 @@ set -e
 
 
 function compile {
-  echo "Compiling benchmarks/$1.dart"
-  dart compile wasm -O4 --no-strip-wasm -o "benchmarks-out/$1.dart2wasm.wasm" "benchmarks/$1.dart"
-  dart compile js -O4 --no-minify -o "benchmarks-out/$1.dart2js.js" "benchmarks/$1.dart"
+  echo "Compiling micro-benchmarks/$1.dart"
+  dart compile wasm -O4 --no-strip-wasm -o "micro-benchmarks-out/$1.dart2wasm.wasm" "micro-benchmarks/$1.dart"
+  dart compile js -O4 --no-minify -o "micro-benchmarks-out/$1.dart2js.js" "micro-benchmarks/$1.dart"
   echo ""
 }
 
-cd benchmarks
+cd micro-benchmarks
 dart pub get
 cd ..
 
-for file in benchmarks/*.dart; do
+for file in micro-benchmarks/*.dart; do
   DART_FILE="$(basename "$file")"
   NAME="${DART_FILE%.dart}"
   compile "$NAME"
